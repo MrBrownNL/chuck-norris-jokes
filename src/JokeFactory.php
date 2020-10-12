87ebc39c77f9bdbf7d707d1faf5cc996542f4d51
Jeroen de Bruijn <jeroen.de.bruijn@conclusion.nl>
2020-10-12 20:38:56 +0200

    Added use of joke api

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -2,26 +2,24 @@
 
 namespace MrBrownNL\ChuckNorrisJokes;
 
+use GuzzleHttp\Client;
+
 class JokeFactory
 {
-    protected $jokes = [
-        'Chuck Norris threw a grenade and killed 50 people, then it exploded.',
-        'Chuck Norris eats habanero peppers to cool down on a hot day.',
-        'Chuck Norris can strangle you with a cordless phone.',
-        'Death once had a near-Chuck-Norris experience.',
-        'Chuck Norris can kill two stones with one bird.',
-        'Chuck Norris doesn\'t dial the wrong number, you pick up the wrong phone.',
-    ];
+    const API_ENDPOINT =  'http://api.icndb.com/jokes/random';
+    protected $client;
 
-    public function __construct(array $jokes = null)
+    public function __construct(Client $client = null)
     {
-        if ($jokes) {
-            $this->jokes = $jokes;
-        }
+        $this->client = $client ?: new Client();
     }
 
     public function getRandomJoke()
     {
-        return $this->jokes[array_rand($this->jokes)];
+        $response = $this->client->get(self::API_ENDPOINT);
+
+        $joke = json_decode($response->getBody()->getContents());
+
+        return $joke->value->joke;
     }
 }
--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -2,6 +2,10 @@
 
 namespace MrBrownNL\ChuckNorrisJokes\Tests;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
 use MrBrownNL\ChuckNorrisJokes\JokeFactory;
 use PHPUnit\Framework\TestCase;
 
@@ -10,28 +14,17 @@ class JokeFactoryTest extends TestCase
     /** @test */
     public function it_returns_a_random_joke()
     {
-        $jokes = new JokeFactory([
-            'This is a joke',
+        $mock = new MockHandler([
+            new Response(200, [], '{ "type": "success", "value": { "id": 281, "joke": "TEST JOKE", "categories": [] } }'),
         ]);
+
+        $handlerStack = HandlerStack::create($mock);
+
+        $client = new Client(['handler' => $handlerStack]);
+
+        $jokes = new JokeFactory($client);
         $joke = $jokes->getRandomJoke();
 
-        $this->assertSame('This is a joke', $joke);
-    }
-
-    /** @test */
-    public function it_returns_a_predefined_joke()
-    {
-        $chuckNorrisJokes = [
-            'Chuck Norris threw a grenade and killed 50 people, then it exploded.',
-            'Chuck Norris eats habanero peppers to cool down on a hot day.',
-            'Chuck Norris can strangle you with a cordless phone.',
-            'Death once had a near-Chuck-Norris experience.',
-            'Chuck Norris can kill two stones with one bird.',
-            'Chuck Norris doesn\'t dial the wrong number, you pick up the wrong phone.',
-        ];
-        $jokes = new JokeFactory();
-        $joke = $jokes->getRandomJoke();
-
-        $this->assertContains($joke, $chuckNorrisJokes);
+        $this->assertSame('TEST JOKE', $joke);
     }
 }
