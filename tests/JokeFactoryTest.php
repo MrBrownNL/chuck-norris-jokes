0cbd180715daa98608019438b60293a376f08ad1
Jeroen de Bruijn <MrBrownNL@users.noreply.github.com>
2020-10-13 17:54:09 +0000

    Apply fixes from StyleCI

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -6,7 +6,6 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
-use MrBrownNL\ChuckNorrisJokes\Facades\ChuckNorris;
 use MrBrownNL\ChuckNorrisJokes\JokeFactory;
 use PHPUnit\Framework\TestCase;
 
